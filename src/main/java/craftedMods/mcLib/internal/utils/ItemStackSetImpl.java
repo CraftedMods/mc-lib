fd911010edb114873809e2fba531926c2f859216
CraftedMods <craftedMods@t-online.de>
2019-09-03 15:59:55 +0200

    Workspace setup

---
+++ src/main/java/craftedMods/mcLib/internal/utils/ItemStackSetImpl.java
@@ -0,0 +1,108 @@
+/*******************************************************************************
+ * Copyright (C) 2019 CraftedMods (see https://github.com/CraftedMods)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package craftedMods.mcLib.internal.utils;
+
+import java.util.*;
+
+import craftedMods.mcLib.api.utils.ItemStackSet;
+import net.minecraft.item.ItemStack;
+
+public class ItemStackSetImpl extends AbstractSet<ItemStack> implements ItemStackSet
+{
+
+    private final boolean isNBTSensitive;
+    private final ArrayList<ItemStackWrapper> innerList = new ArrayList<> ();
+    private final ArrayList<ItemStack> stacksList = new ArrayList<> ();
+
+    public ItemStackSetImpl (ItemStack... stacks)
+    {
+        this (false, stacks);
+    }
+
+    public ItemStackSetImpl (boolean isNBTSensitive, ItemStack... stacks)
+    {
+        this.isNBTSensitive = isNBTSensitive;
+        for (ItemStack stack : stacks)
+        {
+            add (stack);
+        }
+    }
+
+    public ItemStackSetImpl (Collection<? extends ItemStack> stacks)
+    {
+        this (false, stacks);
+    }
+
+    public ItemStackSetImpl (boolean isNBTSensitive, Collection<? extends ItemStack> stacks)
+    {
+        this.isNBTSensitive = isNBTSensitive;
+        addAll (stacks);
+    }
+
+    @Override
+    public Iterator<ItemStack> iterator ()
+    {
+        return stacksList.iterator ();
+    }
+
+    @Override
+    public int size ()
+    {
+        return innerList.size ();
+    }
+
+    @Override
+    public boolean isEmpty ()
+    {
+        return innerList.isEmpty ();
+    }
+
+    public boolean contains (ItemStack stack)
+    {
+        return innerList.contains (new ItemStackWrapper (stack, isNBTSensitive));
+    }
+
+    @Override
+    public boolean add (ItemStack stack)
+    {
+        boolean ret = false;
+        ItemStackWrapper wrapper = new ItemStackWrapper (stack, isNBTSensitive);
+        if (!innerList.contains (wrapper))
+        {
+            ret = innerList.add (wrapper);
+            if (ret)
+            {
+                stacksList.add (stack);
+            }
+        }
+        return ret;
+    }
+
+    public boolean remove (ItemStack stack)
+    {
+        return innerList.remove (new ItemStackWrapper (stack, isNBTSensitive))
+            && stacksList.remove (stack);
+    }
+
+    @Override
+    public void clear ()
+    {
+        innerList.clear ();
+        stacksList.clear ();
+    }
+
+}
---
+++ src/main/java/craftedMods/mcLib/internal/utils/ItemStackWrapper.java
@@ -0,0 +1,112 @@
+/*******************************************************************************
+ * Copyright (C) 2019 CraftedMods (see https://github.com/CraftedMods)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package craftedMods.mcLib.internal.utils;
+
+import net.minecraft.item.*;
+import net.minecraft.nbt.NBTTagCompound;
+
+public class ItemStackWrapper
+{
+
+    private final boolean isNBTSensitive;
+    private final ItemStack stack;
+    private final Item item;
+    private final int damage;
+    private final NBTTagCompound compound;
+
+    public ItemStackWrapper (ItemStack stack)
+    {
+        this (stack, false);
+    }
+
+    public ItemStackWrapper (ItemStack stack, boolean isNBTSensitive)
+    {
+        this.isNBTSensitive = isNBTSensitive;
+        item = stack.getItem ();
+        damage = stack.getItemDamage ();
+        compound = stack.getTagCompound ();
+        this.stack = stack;
+    }
+
+    public Item getItem ()
+    {
+        return item;
+    }
+
+    public int getDamage ()
+    {
+        return damage;
+    }
+
+    public NBTTagCompound getTagCompound ()
+    {
+        return compound;
+    }
+
+    public ItemStack toItemStack ()
+    {
+        return stack;
+    }
+
+    @Override
+    public int hashCode ()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (item == null ? 0 : item.hashCode ());
+        result = prime * result + damage;
+        result = prime * result + (isNBTSensitive ? 1231 : 1237);
+        if (isNBTSensitive)
+        {
+            result = prime * result + (compound == null ? 0 : compound.hashCode ());
+        }
+        return result;
+    }
+
+    @Override
+    public boolean equals (Object obj)
+    {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (this.getClass () != obj.getClass ())
+            return false;
+        ItemStackWrapper other = (ItemStackWrapper) obj;
+        if (item == null)
+        {
+            if (other.item != null)
+                return false;
+        }
+        else if (!item.equals (other.item))
+            return false;
+        if (damage != other.damage)
+            return false;
+        if (isNBTSensitive != other.isNBTSensitive)
+            return false;
+        if (isNBTSensitive)
+            if (compound == null)
+            {
+                if (other.compound != null)
+                    return false;
+            }
+            else if (!compound.equals (other.compound))
+                return false;
+        return true;
+    }
+
+}
